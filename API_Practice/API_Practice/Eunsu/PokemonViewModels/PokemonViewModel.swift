98139a6f5117371991ce868693c1509816621adc
Eunice <jes.e.0927@gmail.com>
2023-11-14 19:08:26 +0900

    ✨ Feat: Set ViewModel
    PokemonManager에서 PokemonList를 받아올 수 있게 한다.
    추후 뷰에서 포켓몬 검색을 할 수 있도록 한다.

--- API_Practice/API_Practice/Eunsu/PokemonViewModels/PokemonViewModel.swift
+++ API_Practice/API_Practice/Eunsu/PokemonViewModels/PokemonViewModel.swift
@@ -8,4 +8,21 @@
 import Foundation
 import SwiftUI
 
-
+final class ViewModel: ObservableObject {
+    private let pokemonManager = PokemonManager()
+    
+    @Published var pokemonList = [Pokemon]()
+    @Published var pokemonDetails: DetailPokemon?
+    @Published var searchText = ""
+    
+    var filteredPokemon: [Pokemon] {
+        return searchText == "" ? pokemonList : pokemonList.filter {
+            $0.name.contains(searchText.lowercased())
+        }
+    }
+    
+    init() {
+        self.pokemonList = pokemonManager.getPokemon()
+        print(self.pokemonList)
+    }
+}
