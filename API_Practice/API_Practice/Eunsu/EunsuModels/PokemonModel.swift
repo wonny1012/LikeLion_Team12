a0304077fc1a459443a7c9a5237e540e1b574834
Eunice <jes.e.0927@gmail.com>
2023-11-14 18:43:03 +0900

    ✨ Feat: Set Model
    - PokemonPage: pokemon.json 파일의 구조
    - Pokemon: PokemonPage 구조체의 result 내부의 구조
    - DeatilPokemon: Pokemon 구조체의 url 내부의 구조 중 deatil view에 사용할 내용

--- API_Practice/API_Practice/Eunsu/EunsuModels/PokemonModel.swift
+++ API_Practice/API_Practice/Eunsu/EunsuModels/PokemonModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
