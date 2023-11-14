69d19cc8ce47dd19a3ae9ea06fff9e702b9a659d
Eunice <jes.e.0927@gmail.com>
2023-11-14 19:44:57 +0900

    📰 Add: Create PokemonView
    포켓몬의 사진과 이름을 보여준다.
    이 뷰는 PokemonDetailView안에서 사용된다.

---
+++ API_Practice/API_Practice/Eunsu/PokemonViews/PokemonView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonView.swift
+//  API_Practice
+//
+//  Created by Eunsu JEONG on 11/14/23.
+//
+
+import SwiftUI
+
+struct PokemonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonView()
+}
