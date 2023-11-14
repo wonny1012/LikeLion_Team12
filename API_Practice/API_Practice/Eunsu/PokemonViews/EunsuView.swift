6d657bd30bd4e8bb86c37cf4f56d6a6ec0460d66
Eunice <jes.e.0927@gmail.com>
2023-11-14 19:32:12 +0900

    🚚 Rename: Add folder Views and move EunsuView into it

---
+++ API_Practice/API_Practice/Eunsu/PokemonViews/EunsuView.swift
@@ -0,0 +1,20 @@
+//
+//  EunsuView.swift
+//  API_Practice
+//
+//  Created by Eunsu JEONG on 11/14/23.
+//
+
+import SwiftUI
+
+struct EunsuView: View {
+    @StateObject var vm = ViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EunsuView()
+}
