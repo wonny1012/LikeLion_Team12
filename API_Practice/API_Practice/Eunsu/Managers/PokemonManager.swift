4d28dc42036372e59635770f20b4722973b82762
Eunice <jes.e.0927@gmail.com>
2023-11-14 18:58:58 +0900

    ✨ Feat: Add functions to read data from pokemon.json and url
    - getPokemon(): read initial state of pokemon json
    - getDetailedPokemon(): get details for each pokemon

--- API_Practice/API_Practice/Eunsu/Managers/PokemonManager.swift
+++ API_Practice/API_Practice/Eunsu/Managers/PokemonManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class PokemonManager {
+    // read initial state of pokemon json
+    func getPokemon() -> [Pokemon] {
+        let data: PokemonPage = Bundle.main.decode(file: "pokemon.json")
+        let pokemon: [Pokemon] = data.results
+        
+        return pokemon
+    }
+    
+    // get details for each pokemon
+    func getDetailedPokemon(id: Int, _ completion: @escaping(DetailPokemon) -> ()) {
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/",
+                              model: DetailPokemon.self) { data in
+            completion(data)
+        } failure: { error in
+            print(error)
+        }
+    }
+}
