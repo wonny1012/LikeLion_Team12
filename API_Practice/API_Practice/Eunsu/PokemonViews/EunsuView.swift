14040631c63ef3c13debf8053900617f8101fa5e
Eunice <jes.e.0927@gmail.com>
2023-11-14 20:30:57 +0900

    ✨ Feat: Set EunsuView to show PokemonList with LazyVGrid
    LazyVGrid로 ViewModel에서 filter된 PokemonList를 불러온다.
    filter에 아무 값이 입력되지 않으면 모든 리스트가 나온다.
    각 셀은 해당하는 포켓몬의 DetailView로 연결된다.

--- API_Practice/API_Practice/Eunsu/PokemonViews/EunsuView.swift
+++ API_Practice/API_Practice/Eunsu/PokemonViews/EunsuView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct EunsuView: View {
     @StateObject var vm = ViewModel()
     
+    private let adaptveColumns = [
+        GridItem(.adaptive(minimum: 150)) //앱을 가로로 돌리면 줄이 눌어난다.
+    ]
+    
     var body: some View {
         NavigationView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            ScrollView {
+                LazyVGrid(columns: adaptveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+            }
         }
         .environmentObject(vm)
     }
