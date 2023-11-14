cf85cc192c8b4a9b0bddcc1d7f0e291fc3bbd1e0
Eunice <jes.e.0927@gmail.com>
2023-11-14 19:32:49 +0900

    📰 Add: Add DetailView

---
+++ API_Practice/API_Practice/Eunsu/PokemonViews/PokemonDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailView.swift
+//  API_Practice
+//
+//  Created by Eunsu JEONG on 11/14/23.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetailView()
+}
