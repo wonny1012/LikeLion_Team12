e0482b777053efe9d8f8f8908504625b40076f01
Eunice <jes.e.0927@gmail.com>
2023-11-14 20:14:51 +0900

    ✨ Feat: Prepare to get AsyncImage and Pokemon name
    AsyncImage로 포켓몬 이미지를 받아온다.
    AsyncImage의 url에는 ViewModel의 getPokemonIndex를 통해 포켓몬 번호를 받는다.
    
    Text에는 해당 포켓몬의 이름을 보여준다.

--- API_Practice/API_Practice/Eunsu/PokemonViews/PokemonView.swift
+++ API_Practice/API_Practice/Eunsu/PokemonViews/PokemonView.swift
@@ -13,7 +13,23 @@ struct PokemonView: View {
     let dimensions: Double = 140
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AsyncImage(url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(vm.getPokemonIndex(pokemon: pokemon)).png")) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: dimensions, height: dimensions)
+            } placeholder: {
+                ProgressView()
+                    .frame(width: dimensions, height: dimensions)
+            }
+            .background(.thinMaterial)
+            .clipShape(Circle())
+            
+            Text("\(pokemon.name.capitalized)")
+                .font(.system(size: 16, weight: .regular, design: .monospaced))
+                .padding(.bottom, 20)
+        }
     }
 }
 
