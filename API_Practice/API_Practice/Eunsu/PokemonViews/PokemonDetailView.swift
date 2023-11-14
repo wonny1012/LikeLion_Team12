6b24c60683cd6bb06806f4734b07c2bbdbbc3a03
Eunice <jes.e.0927@gmail.com>
2023-11-14 19:43:08 +0900

    ✨ Feat: Set Detail View
    디테일 뷰를 구성한다.
    - PokemonView()는 아직 생성되지 않은 뷰 이므로 주석처리한다.
    - VStack안에 ViewModel에서 PokemonDetail을 받아온다.
    - 디테일 뷰가 나타나면 ViewModel에서 detail을 불러온다.

--- API_Practice/API_Practice/Eunsu/PokemonViews/PokemonDetailView.swift
+++ API_Practice/API_Practice/Eunsu/PokemonViews/PokemonDetailView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct PokemonDetailView: View {
+    @EnvironmentObject var vm: ViewModel //이미 존재하는 것을 언급하는것이므로 초기화하지 않는다.
+    let pokemon: Pokemon
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+//            PokemonView()
+            
+            VStack(spacing: 10) {
+                Text("**ID**: \(vm.pokemonDetails?.id ?? 0)")
+                Text("**Weight**: \(vm.formatHW(value: vm.pokemonDetails?.weight ?? 0)) kg")
+                Text("**Height**: \(vm.formatHW(value: vm.pokemonDetails?.height ?? 0)) m")
+            }
+        }
+        .onAppear {
+            vm.getDetails(pokemon: pokemon)
+        }
     }
 }
 
 #Preview {
-    PokemonDetailView()
+    PokemonDetailView(pokemon: Pokemon.samplePokemon).environmentObject(ViewModel())
 }
