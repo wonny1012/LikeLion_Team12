1e6b82b2a4ab74e66503478dc228ad647b17ad0b
Eunice JEONG <106911494+Eunice0927@users.noreply.github.com>
2023-11-16 02:26:41 +0900

    🐞 Fix: Update PokemonManager.swift
    Conflict 해결 과정에서 잘못 수정한 코드를 되돌린다.

--- API_Practice/API_Practice/Eunsu/Managers/PokemonManager.swift
+++ API_Practice/API_Practice/Eunsu/Managers/PokemonManager.swift
@@ -21,16 +21,17 @@ class PokemonManager {
         Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/",
                               model: DetailPokemon.self) { data in
             completion(data)
+            print(data)
         } failure: { error in
             print(error)
         }
     }
     
-    func getPokemonSpecies(id: Int, _ completion: @escaping(Genera) -> ()) {
-        Bundle.main.fetchData(url: "pokeapi.co/api/v2/pokemon-species/\(id)/",
-                              model: Genera.self) { data in
+    func getPokemonSpecies(id: Int, _ completion: @escaping(Generas) -> ()) {
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon-species/\(id)/",
+                              model: Generas.self) { data in
             completion(data)
-            print(data)
+            print("\(#function): \(data.genera)")
         } failure: { error in
             print(error)
         }
