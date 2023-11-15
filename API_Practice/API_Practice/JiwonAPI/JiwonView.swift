40cfad0230e8950e7cc89c0e8860f54137039a78
wonny1012 <rnjswldnjs1012@gmail.com>
2023-11-15 17:00:40 +0900

    feature YouTube연결

--- API_Practice/API_Practice/JiwonAPI/JiwonView.swift
+++ API_Practice/API_Practice/JiwonAPI/JiwonView.swift
@@ -15,12 +15,23 @@ struct JiwonView: View {
         NavigationView {
             List(songs, id: \.id) { song in
                 HStack {
+                    //수정할 예정
+//질문 예정  Image("https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/63/4a/b8/634ab8d4-abbd-d00a-8913-3048bc89cb4d/075679749123_01.jpg/60x60bb.jpg")
                     Image(systemName: "airplane")
                         .frame(width: 100, height: 100)
                     VStack(alignment: .leading) {
                         Text(song.trackName)
                         Text(song.artistName)
-//
+                    }
+                    Spacer()
+                    //클릭을 했을 때 youtube로 넘어가게 만들었습니다.
+                    Link(destination: URL(string: "https://www.youtube.com/results?search_query=\(song.trackName)+\(song.artistName)")
+                         ?? URL(string: "https://www.youtube.com")!) {
+                            Image(systemName: "star")
+                                .background()
+                                .clipShape(Circle())
+                                .foregroundColor(.black)
+                        
                     }
                 }
             }
@@ -57,25 +68,6 @@ struct ArtworkView: View {
         .padding(.trailing, 5)
     }
 }
-
-//struct SongView: View {
-//    @Binding var song: [Song]
-//    
-//    var body: some View {
-//        HStack {
-//            ArtworkView(image: ")
-//                .padding(.trailing)
-//            VStack(alignment: .leading) {
-//                Text(song.trackName)
-//                Text(song.artistName)
-//                    .font(.footnote)
-//                    .foregroundColor(.gray)
-//            }
-//        }
-//        .padding()
-//    }
-//}
-
 
 
 #Preview {
