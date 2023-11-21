6b73c908d52156aaf361fb2fe2171505c80a84f2
burst-cannon <qlrmr111@naver.com>
2023-11-22 06:34:14 +0900

    🎨 Design: UI 만들어보기

--- API_Practice/API_Practice/UnkiView.swift
+++ API_Practice/API_Practice/UnkiView.swift
@@ -7,11 +7,53 @@
 
 import SwiftUI
 
+struct Comment: Codable, Identifiable {
+    var id: Int
+    var postId: Int
+    var name: String
+    var email: String
+    var body: String
+}
+
 struct UnkiView: View {
+@State private var comments: [Comment] = []
+    
     var body: some View {
-        VStack {
-            
+        NavigationView {
+            List(comments) { comment in
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("이름: \(comment.name)")
+                        .font(.headline)
+                    Text("이메일: \(comment.email)")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Text("내용: \(comment.body)")
+                        .font(.body)
+                        .foregroundColor(.black)
+                }
+            }
+            .onAppear(perform: loadData)
+            .navigationBarTitle("댓글 목록")
         }
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/comments") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                do {
+                    let decodedData = try JSONDecoder().decode([Comment].self, from: data)
+                    DispatchQueue.main.async {
+                        self.comments = decodedData
+                    }
+                } catch {
+                    print("Error decoding JSON: \(error)")
+                }
+            }
+        }.resume()
     }
 }
 
