2f1888fc192543ea49972dcb97c0dd243bf71917
Im-loki <al4126@naver.com>
2023-11-25 14:59:19 +0900

    GitHubUser API 호출

---
+++ API_Practice/API_Practice/Loki/GitHubUsersModel.swift
@@ -0,0 +1,8 @@
+//
+//  GitHubUsersModel.swift
+//  API_Practice
+//
+//  Created by 양주원 on 11/25/23.
+//
+
+import Foundation
---
+++ API_Practice/API_Practice/Loki/LokiView.swift
@@ -0,0 +1,18 @@
+//
+//  LokiView.swift
+//  API_Practice
+//
+//  Created by 양주원 on 11/25/23.
+//
+
+import SwiftUI
+
+struct LokiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LokiView()
+}
