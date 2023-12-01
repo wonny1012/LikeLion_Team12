a85bb04e83b5caf73cd57b5ca298aa81e0481c67
burst-cannon <qlrmr111@naver.com>
2023-12-01 15:01:35 +0900

    ✨ Feat: 날짜가 오늘일경우 경고메시지? 띄우기

--- API_Practice/API_Practice/UnkiView.swift
+++ API_Practice/API_Practice/UnkiView.swift
@@ -10,38 +10,66 @@ import SwiftUI
 struct UnkiView: View {
     @State private var DailyBoxOfficeLists: [DailyBoxOfficeList] = []
     @State public var now = Date()
+    @State private var isLoadingData = false
     
     var body: some View {
         NavigationView {
-            List(DailyBoxOfficeLists, id: \.rank) { movies in
-                NavigationLink(destination: UnkiDetailView(DailyBoxOfficeLists: movies)) {
-                    VStack {
-                        Text("\(movies.rank). \(movies.movieNm)")
-                    }
-                }
-            }
-            .onAppear(perform: loadData)
-            .toolbar {
-                DatePicker(
+            if !isLoadingData {
+                VStack {
+                    DatePicker(
                         "",
                         selection: $now,
                         displayedComponents: [.date]
-                )
-                .onChange(of: now) { // loadData로 now값 전달
-                     loadData()
-                 }
+                    )
+                    .onChange(of: now) { // loadData로 now값 전달
+                        loadData()
+                    }
+                    HStack {
+                        Text("\("MM월dd일".stringFromDate(now: now)) 영화 순위")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(15)
+                    Text("오늘의 데이터는 수집 중입니다.\n 날짜를 과거로 바꿔주세요.")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+            } else {
+                List(DailyBoxOfficeLists, id: \.rank) { movies in
+                    NavigationLink(destination: UnkiDetailView(DailyBoxOfficeLists: movies)) {
+                        VStack {
+                            Text("\(movies.rank). \(movies.movieNm)")
+                        }
+                    }
+                }
+                .onAppear(perform: loadData)
+                .toolbar {
+                    DatePicker(
+                        "",
+                        selection: $now,
+                        displayedComponents: [.date]
+                    )
+                    .onChange(of: now) { // loadData로 now값 전달
+                        loadData()
+                    }
+                }
+                .navigationTitle("\("MM월dd일".stringFromDate(now: now)) 영화 순위")
             }
-            .navigationTitle("\("MM월dd일".stringFromDate(now: now)) 영화 순위")
         }
     }
     
     func loadData() {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyyMMdd"
+        let targetDt = formatter.string(from: now)
         
-        guard let url = URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=\(formatter.string(from: now))") else {
+        guard let url = URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=\(targetDt)") else {
             return
         }
+        
+        isLoadingData = true
         
         URLSession.shared.dataTask(with: url) { data, _, error in
             if let data = data {
@@ -75,7 +103,6 @@ struct UnkiDetailView: View { // 디테일 뷰
                         .font(.title2)
                     Text("누적 관객수: \(DailyBoxOfficeLists.audiAcc)명")
                         .font(.title2)
-                    
                 }
                 Spacer()
             }
